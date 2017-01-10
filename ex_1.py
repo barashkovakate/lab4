763e6c42bf13abf72406fe8eb62098bc9fcf5cf2
barashkovakate <berillka@mail.ru>
2017-01-10 12:43:08 +0300

    Added lab4 scripts

--- ex_1.py
+++ ex_1.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
-from librip.gens import field
+from librip.gens import field, gen_random
 
 goods = [
     {'title': 'Ковер', 'price': 2000, 'color': 'green'},
+    {'title': 'Кресло', 'price': 1500, 'color': 'brown'},
     {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'},
     {'title': 'Стелаж', 'price': 7000, 'color': 'white'},
+    {'title': None, 'price': 100, 'color': 'blue'},
     {'title': 'Вешалка для одежды', 'price': 800, 'color': 'white'}
 ]
 
 # Реализация задания 1
+
+print(list(field(goods, 'title')))
+print(list(field(goods, 'title', 'price')))
+
+print(list(gen_random(1,3,5)))
+
--- ex_6.py
+++ ex_6.py
@@ -6,12 +6,13 @@ from librip.decorators import print_result
 from librip.gens import field, gen_random
 from librip.iterators import Unique as unique
 
-path = None
+path = "data_light.json"
+
 
 # Здесь необходимо в переменную path получить
 # путь до файла, который был передан при запуске
 
-with open(path) as f:
+with open(path, encoding="utf8") as f:
     data = json.load(f)
 
 
@@ -23,22 +24,23 @@ with open(path) as f:
 
 @print_result
 def f1(arg):
-    raise NotImplemented
+     return sorted(unique(field(arg, 'job-name'), ignore_case=True), key=str.lower)
 
 
 @print_result
 def f2(arg):
-    raise NotImplemented
+    return list(filter(lambda x: str(x).lower().startswith('программист'), arg))
 
 
 @print_result
 def f3(arg):
-    raise NotImplemented
+     return ["{} с опытом Python".format(i) for i in arg]
 
 
 @print_result
 def f4(arg):
-    raise NotImplemented
+    return ["{}, зарплата {} руб.".format(work, salary) for (work, salary) in
+            zip(arg, gen_random(100000, 200000, len(arg)))]
 
 
 with timer():
